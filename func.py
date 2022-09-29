8e3480b90ed2e168895fdea52807c04680ee307a
Kartik-Agra-wal <87862080+Kartik-Agra-wal@users.noreply.github.com>
2022-09-29 22:21:48 +0530

    Add files via upload
    The main file is to be run, given an extra argument show_list (default=false) if want to show list of ships and rank them.

---
+++ func.py
@@ -0,0 +1,106 @@
+from email import message
+import matplotlib.pyplot as plt
+import numpy as np
+from arpaocalc import Ship, ARPA_calculations
+from tkinter import *
+
+
+def get_arrow(angle):
+    a = np.deg2rad(angle)
+    ar = np.array([[-.25,-.5],[.25,-.5],[0,.5],[-.25,-.5]]).T
+    rot = np.array([[np.cos(a),np.sin(a)],[-np.sin(a),np.cos(a)]])
+    return np.dot(rot,ar).T 
+
+
+
+def test(a,id_main,show_list = False,theta=0):
+    plt.close()
+
+    id_list = []
+    tcpa_dict = dict()
+    ship_ls = []
+    for msg in a:
+        if msg["mmsi"] not in id_list:
+            ship_ls.append(Ship(msg["mmsi"],(msg["lon"],msg["lat"]),msg["speed"],msg["course"]))
+            id_list.append(msg['mmsi'])
+            if msg['mmsi'] == id_main:
+                objectA = ship_ls[-1]
+    objectA.heading += theta
+    plt.xlim(xmin=objectA.position[0]-0.25,xmax=objectA.position[0]+0.25)
+    plt.ylim(ymin=objectA.position[1]-0.25,ymax=objectA.position[1]+0.25)
+    plt.plot(objectA.position[0],objectA.position[1] ,marker =get_arrow(90-objectA.heading),color = 'blue', markersize=15)
+    plt.grid()
+
+
+    txt = 'ship list \n'
+
+    for i in range(4):
+
+        c1 = plt.Circle((objectA.position[0],objectA.position[1] ),(i+1)*0.08 ,fill = False )
+        c1.set_edgecolor("green")
+        plt.gcf().gca().add_artist(c1)    
+    name = 'INS MUMBAI'
+    plt.text(objectA.position[0]+0.01,objectA.position[1],name)
+
+
+    for i in range(0,len(ship_ls)):
+        
+        if id_list[i] != id_main:
+            id = id_list[i]
+            objectB = ship_ls[i]
+            results = ARPA_calculations(objectA, objectB,m=True, posAatcpa = True, posBatcpa= True)
+            str1 = 'cpa:' +str(round(results['cpa'],3)) + 'tcpa: '+ str(round(results['tcpa'],3))
+            plt.plot(objectB.position[0],objectB.position[1] ,marker =get_arrow(90 -objectB.heading),color = 'orange', markersize=15)
+            plt.text(objectB.position[0],objectB.position[1],str1)
+            txt += str(id_list[i]) +" " + str1 + "\n"+ results['status']+'\n'
+            tcpa_dict[id] = results['cpa']
+
+    dic2=dict(sorted(tcpa_dict.items(),key= lambda x:x[1]))
+    res = list(dic2.keys())[0]
+    for i in range(len(id_list)):
+        if id_list[i]==res:
+            objectB = ship_ls[i]
+            results = ARPA_calculations(objectA, objectB,m=True, posAatcpa = True, posBatcpa= True)
+            try:
+                plt.plot(results['coord'][0],results['coord'][1] ,marker =get_arrow(90 -objectA.heading),color = 'red', markersize=15)
+                plt.text(results['coord'][0]+0.01,results['coord'][1],name)
+                plt.plot(results['coord'][2],results['coord'][3] ,marker =get_arrow(90 -objectB.heading),color = 'black', markersize=15)
+                plt.plot([results['coord'][0],results['coord'][2]],[results['coord'][1],results['coord'][3]],'m--')
+                plt.plot([objectB.position[0],results['coord'][2]],[objectB.position[1],results['coord'][3]],'k--')
+                plt.plot([objectA.position[0],results['coord'][0]],[objectA.position[1],results['coord'][1]],'r--')
+            except:
+                pass
+            c = plt.Circle((objectB.position[0], objectB.position[1] ),0.008 ,fill = False )
+            c.set_edgecolor("red")
+            plt.gcf().gca().add_artist(c)
+
+
+    if show_list:
+        ws = Tk()
+        ws.title('SHIPS LIST')
+        ws.geometry('400x300')
+        ws.config(bg='#A67449')
+        message = txt
+        text_box = Text(
+            ws,
+            height=26,
+            width=80
+        )
+        text_box.pack(expand=True)
+
+        text_box.insert('end', message)
+
+        ws2 = Tk()
+        ws2.title('rank')
+        ws2.geometry('400x300')
+        ws2.config(bg='#A67449')
+        message = dic2
+        text_box2 = Text(
+            ws2,
+            height=26,
+            width=80
+        )
+        text_box2.pack(expand=True)
+
+        text_box2.insert('end', message)
+    plt.show()
---
+++ main.py
@@ -0,0 +1,45 @@
+from email import message
+import matplotlib.pyplot as plt
+import numpy as np
+from arpaocalc import Ship, ARPA_calculations
+from tkinter import *
+from tkinter import ttk
+from func import test
+
+
+id = 419565453
+
+a= [{'msg_type': 1, 'repeat': 0, 'mmsi': 419001261, 'status': '<NavigationStatus.Moored: 5>', 'turn': None, 'speed': 2.9, 'accuracy': False, 'lon': 72.882983, 'lat': 18.867343, 'course': 24.2, 'heading': 511, 'second': 1, 'maneuver': 0, 'spare_1': b'\x00', 'raim': False, 'radio': 49315},
+{'msg_type': 1, 'repeat': 0, 'mmsi': 419565453, 'status': '<NavigationStatus.UnderWayUsingEngine: 0>', 'turn': None, 'speed': 3, 'accuracy': False, 'lon': 73.05928, 'lat': 19.029358, 'course': 270.0, 'heading': 511, 'second': 3, 'maneuver': 0, 'spare_1': b'\x00', 'raim': True, 'radio': 262144},
+{'msg_type': 1, 'repeat': 0, 'mmsi': 419565453, 'status': '<NavigationStatus.UnderWayUsingEngine: 0>', 'turn': None, 'speed': 3, 'accuracy': False, 'lon': 73.05928, 'lat': 19.029358, 'course': 360.0, 'heading': 511, 'second': 4, 'maneuver': 0, 'spare_1': b'\x00', 'raim': True, 'radio': 262144},
+{'msg_type': 1, 'repeat': 0, 'mmsi': 419000482, 'status': '<NavigationStatus.UnderWayUsingEngine: 0>', 'turn': 0.0, 'speed': 1, 'accuracy': False, 'lon': 72.915798, 'lat': 18.949165, 'course': 50.4, 'heading': 317, 'second': 4, 'maneuver': 0, 'spare_1': b'\x00', 'raim': False, 'radio': 49376},
+{'msg_type': 1, 'repeat': 0, 'mmsi': 419001261, 'status': '<NavigationStatus.Moored: 5>', 'turn': None, 'speed': 3.0, 'accuracy': False, 'lon': 72.88296, 'lat': 18.86736, 'course': 35.7, 'heading': 511, 'second': 3, 'maneuver': 0, 'spare_1': b'\x00', 'raim': False, 'radio': 114852},
+{'msg_type': 1, 'repeat': 0, 'mmsi': 419565453, 'status': '<NavigationStatus.UnderWayUsingEngine: 0>', 'turn': None, 'speed': 2, 'accuracy': False, 'lon': 73.05928, 'lat': 19.029353, 'course': 360.0, 'heading': 511, 'second': 5, 'maneuver': 0, 'spare_1': b'\x00', 'raim': True, 'radio': 262144},
+{'msg_type': 3, 'repeat': 0, 'mmsi': 419001261, 'status': '<NavigationStatus.Moored: 5>', 'turn': None, 'speed': 2.9, 'accuracy': False, 'lon': 72.88295, 'lat': 18.867372, 'course': 32.2, 'heading': 511, 'second': 4, 'maneuver': 0, 'spare_1': b'\x00', 'raim': False, 'radio': 0},
+{'msg_type': 1, 'repeat': 0, 'mmsi': 419001585, 'status': '<NavigationStatus.UnderWayUsingEngine: 0>', 'turn': 0.0, 'speed': 1.0, 'accuracy': False, 'lon': 72.954813, 'lat': 18.972742, 'course': 26.4, 'heading': 33, 'second': 4, 'maneuver': 0, 'spare_1': b'\x00', 'raim': False, 'radio': 98547},
+{'msg_type': 1, 'repeat': 0, 'mmsi': 419565453, 'status': '<NavigationStatus.UnderWayUsingEngine: 0>', 'turn': None, 'speed': 2, 'accuracy': False, 'lon': 73.05928, 'lat': 19.029353, 'course': 360.0, 'heading': 511, 'second': 6, 'maneuver': 0, 'spare_1': b'\x00', 'raim': True, 'radio': 262144},
+{'msg_type': 1, 'repeat': 0, 'mmsi': 419565453, 'status': '<NavigationStatus.UnderWayUsingEngine: 0>', 'turn': None, 'speed': 1, 'accuracy': False, 'lon': 73.059287, 'lat': 19.029353, 'course': 360.0, 'heading': 511, 'second': 7, 'maneuver': 0, 'spare_1': b'\x00', 'raim': True, 'radio': 262144},
+{'msg_type': 1, 'repeat': 0, 'mmsi': 419001261, 'status': '<NavigationStatus.Moored: 5>', 'turn': None, 'speed': 2.8, 'accuracy': False, 'lon': 72.882935, 'lat': 18.867393, 'course': 31.0, 'heading': 511, 'second': 6, 'maneuver': 0, 'spare_1': b'\x00', 'raim': False, 'radio': 3508},
+{'msg_type': 1, 'repeat': 0, 'mmsi': 419767000, 'status': '<NavigationStatus.UnderWayUsingEngine: 0>', 'turn': 0.0, 'speed': 7.6, 'accuracy': True, 'lon': 72.8956, 'lat': 18.935967, 'course': 238.0, 'heading': 240, 'second': 6, 'maneuver': 0, 'spare_1': b'\x00', 'raim': False, 'radio': 22072},
+{'msg_type': 1, 'repeat': 0, 'mmsi': 419575000, 'status': '<NavigationStatus.UnderWayUsingEngine: 0>', 'turn': None, 'speed': 1, 'accuracy': False, 'lon': 72.971545, 'lat': 19.015558, 'course':48.9, 'heading': 511, 'second': 6, 'maneuver': 0, 'spare_1': b'@', 'raim': False, 'radio': 49308},
+{'msg_type': 1, 'repeat': 0, 'mmsi': 419956823, 'status': '<NavigationStatus.UnderWaySailing: 8>', 'turn': None, 'speed': 1, 'accuracy': False, 'lon': 73.248547, 'lat': 19.118387, 'course': 236.8, 'heading': 511, 'second': 11, 'maneuver': 0, 'spare_1': b'\x00', 'raim': False, 'radio': 254}]
+
+
+# test(a)
+win= Tk()
+win.geometry("750x250")
+Button(win, text= "Show Graph", command=lambda:  test(a,id_main=id)).pack(pady=20)
+b1 = ttk.Button(win, text= "Change orientation +30", command=lambda: test(a,id,theta=30))
+b1.place(x=200, y=100)
+b2 = ttk.Button(win, text= "Change orientation +60", command=lambda: test(a,id,theta=60))
+b2.place(x=200, y=150)  
+b3 = ttk.Button(win, text= "Change orientation -30", command=lambda: test(a,id,theta =-30))
+b3.place(x=450, y=100)
+b4 = ttk.Button(win, text= "Change orientation -60", command=lambda: test(a,id,theta =-60))
+b4.place(x=450, y=150)
+win.mainloop()
+
+
+
+
+    
